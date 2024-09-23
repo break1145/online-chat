2c423c3425aad5d761094e00861119c805324c5a
break1145 <breakgoogle@163.com>
2024-09-23 22:23:58 +0800

    环境配置

---
+++ mallchat-chat-server/src/main/java/com/chat/server/common/MallchatCustomApplication.java
@@ -0,0 +1,17 @@
+package com.chat.server.common;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+
+@SpringBootApplication(scanBasePackages = {"com.chat.server"})
+@ServletComponentScan
+public class MallchatCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallchatCustomApplication.class,args);
+    }
+
+}
---
+++ mallchat-chat-server/src/main/java/com/chat/server/common/websocket/NettyWebSocketServerHandler.java
@@ -0,0 +1,14 @@
+package com.chat.server.common.websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+        String text = textWebSocketFrame.text();
+        System.out.println(text);
+    }
+}
