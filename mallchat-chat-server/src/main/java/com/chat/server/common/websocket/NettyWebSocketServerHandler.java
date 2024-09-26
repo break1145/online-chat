ebbdc9966d713e772400a33792237fb63b9d8ee0
break1145 <breakgoogle@163.com>
2024-09-26 16:55:13 +0800

    websocket完成

--- mallchat-chat-server/src/main/java/com/chat/server/common/websocket/NettyWebSocketServerHandler.java
+++ mallchat-chat-server/src/main/java/com/chat/server/common/websocket/NettyWebSocketServerHandler.java
@@ -1,14 +1,69 @@
 package com.chat.server.common.websocket;
 
+
+import cn.hutool.json.JSONUtil;
+import com.chat.server.common.user.domain.enums.WSReqTypeEnum;
+import com.chat.server.common.user.domain.vo.WSBaseReq;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
+@Sharable
 public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
+    /**
+     * 心跳检查
+     * @param ctx
+     * @param evt
+     * @throws Exception
+     */
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
-        String text = textWebSocketFrame.text();
-        System.out.println(text);
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            // 读空闲
+            if (event.state() == IdleState.READER_IDLE) {
+                userOffLine(ctx);
+            }
+        } else if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+            System.out.println("握手完成");
+
+        }
+        super.userEventTriggered(ctx, evt);
+    }
+    // 关闭连接
+    private void userOffLine(ChannelHandlerContext ctx) {
+        ctx.channel().close();
+    }
+
+
+    /**
+     * 读取客户端发送的请求报文
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        WSBaseReq wsBaseReq = JSONUtil.toBean(msg.text(), WSBaseReq.class);
+        WSReqTypeEnum wsReqTypeEnum = WSReqTypeEnum.of(wsBaseReq.getType());
+        switch (wsReqTypeEnum) {
+            case LOGIN:
+//                this.webSocketService.handleLoginReq(ctx.channel());
+                log.info("请求二维码 = " + msg.text());
+                break;
+            case HEARTBEAT:
+                break;
+            default:
+                log.info("未知类型");
+        }
     }
 }
